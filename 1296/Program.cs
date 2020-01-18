3d56db36b5244b79355c9d05b732d34c1c9a1d4c
Grigoriy Voronin <grigoriy.1998@mail.ru>
2020-01-18 07:47:29 +0500

    timus.1

---
+++ 1296/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace col3_1296
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sumMax = 0;
+            var n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+            for (int i =0; i<n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            if (arr.Length != 0)
+            {
+                var sum = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (sum > sumMax)
+                        sumMax = sum;
+                    if (sum + arr[i] > 0)
+                        sum += arr[i];
+                    else
+                        sum = 0;
+                }
+                if (sum > sumMax)
+                    sumMax = sum;
+            }
+                Console.WriteLine(sumMax);
+        }
+    }
+}
---
+++ 1592/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace krugi
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var timeInSec = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                var d = Console.ReadLine().Split(':');
+                timeInSec[i] = int.Parse(d[0]) * 3600 + int.Parse(d[1]) * 60 + int.Parse(d[2]);
+            }
+            Array.Sort(timeInSec);
+
+            var timeInRoad = new long[n];
+            for (int i = 1; i < n; i++)
+                timeInRoad[0] += i * (timeInSec[i] - timeInSec[i - 1]);
+
+            if (n > 1)
+                timeInRoad[1] = timeInRoad[0] - (timeInSec[n - 1] - timeInSec[0]) + (n - 1) * (43200 - timeInSec[n - 1] + timeInSec[0]);
+
+            for (int i = 2; i < n; i++)
+                timeInRoad[i] = timeInRoad[i - 1] - (43200 - timeInSec[i - 1] + timeInSec[i - 2]) + (n - 1) * (timeInSec[i - 1] - timeInSec[i - 2]);
+
+            var an = (Array.IndexOf(timeInRoad, timeInRoad.Min()) - 1);
+            var index = an == -1 ? n - 1 : an;
+
+            var sec = timeInSec[index] % 60;
+            var min = timeInSec[index] % 3600 / 60;
+            var hours = timeInSec[index] / 3600;
+            Console.WriteLine("{0:0}:{1:00}:{2:00}", hours, min, sec);
+        }
+    }
+}

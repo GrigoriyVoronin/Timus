add7915b88c0957cd63174f046241f8ccbb8cf70
GrigoriyVoronin <grigoriy.1998@mail.ru>
2020-06-14 21:14:44 +0500

    solved 1001

---
+++ 1001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1000
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var numbers = new List<char>(128);
+            var delimetrs = new[] {' ', '\r', '\n'};
+            var output = new List<double>(128);
+            for (var i = 0; i < 256 * 1024; i++)
+            {
+                var simbolInt = Console.Read();
+                if (simbolInt == -1)
+                    break;
+
+                var simbolCh = (char) simbolInt;
+                while (!delimetrs.Contains(simbolCh))
+                {
+                    numbers.Add(simbolCh);
+                    simbolInt = Console.Read();
+                    if (simbolInt == -1)
+                        break;
+                    simbolCh = (char) simbolInt;
+                }
+
+                if (numbers.Count != 0)
+                {
+                    output.Add(Math.Round(Math.Sqrt(double.Parse(new string(numbers.ToArray()))), 4));
+                    numbers = new List<char>(128);
+                }
+            }
+
+            output.Reverse();
+            foreach (var numb in output) Console.WriteLine(numb);
+        }
+    }
+}
---
+++ 1713/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1713
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLength = int.Parse(Console.ReadLine());
+            var commands = new HashSet<string>();
+            for (var i = 0; i < inputLength; i++)
+            {
+                commands.Add(Console.ReadLine());
+            }
+        }
+    }
+}
